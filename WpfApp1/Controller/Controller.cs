9340341be6c56e0e626dcf2beae8eb50b0d5ef5e
Mikoto Shinozaki <sernfst58@mail.ru>
2019-04-08 20:04:01 +0300

    1

--- WpfApp1/Controller/Controller.cs
+++ WpfApp1/Controller/Controller.cs
@@ -1,5 +1,9 @@
 ﻿using System . Collections . Generic;
 using System.IO;
+using System.Security.Cryptography;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using WpfApp1.File;
 using WpfApp1.View;
 
@@ -7,6 +11,8 @@ namespace WpfApp1 . Controller
 {
     class Controller
     {
+
+        private string path;
 
         private IData person_file;
         private MainWindow window;
@@ -17,8 +23,20 @@ namespace WpfApp1 . Controller
             this.window = window;
             this.person_file = person_file;
             this.view = view;
+
+            this . window . list_Item_Selected += Window_list_Item_Selected; ; 
         }
 
+        private void Window_list_Item_Selected( object sender , System . Windows . Input . MouseButtonEventArgs e )
+        {
+            var item = ( ( FrameworkElement ) e . OriginalSource ) . DataContext as View.View;
+
+            if ( item != null )
+            {
+                path += item.Title + "\\";
+                printFile (path);
+            }
+        }
 
         public void printFile(string path)
         {
--- WpfApp1/View/MainWindow.xaml.cs
+++ WpfApp1/View/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using System . Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
 using WpfApp1.File;
 using WpfApp1.View;
 
@@ -9,6 +11,8 @@ namespace WpfApp1
     /// </summary>
     public partial class MainWindow : Window
     {
+        public event MouseButtonEventHandler list_Item_Selected;
+
         public MainWindow()
         {
             InitializeComponent ( );
@@ -20,6 +24,8 @@ namespace WpfApp1
             Controller.Controller controller = new Controller.Controller(this, personFile, view);
 
             controller.printFile("\\");
+            
+            file_list.MouseDoubleClick+=list_Item_Selected;
         }
     }
 }
