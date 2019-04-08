0f921ebad223490672d7ae37e171205c6a081c11
Mikoto Shinozaki <sernfst58@mail.ru>
2019-04-08 18:59:40 +0300

    1

--- WpfApp1/Controller/Controller.cs
+++ WpfApp1/Controller/Controller.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System . Collections . Generic;
+﻿using System . Collections . Generic;
 using System.IO;
-using System . Linq;
-using System . Text;
-using System . Threading . Tasks;
-using System.Windows;
 using WpfApp1.File;
 using WpfApp1.View;
 
